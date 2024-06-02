267f1433193a9739505d3f5753a07e91d71710db
Roman Khabarov <roman@ga-games.com>
2024-06-02 17:56:59 +0200

    fix

--- prepare-scripts/init-dev-env.sh
+++ prepare-scripts/init-dev-env.sh
@@ -6,31 +6,13 @@ mkdir -p /workspace
 
 if ! [ -d $DIR/wfs ]; then
     echo "Clone repository"
-    git clone git@git.globalart.dev:globalart/wfs.git
+    git clone git@github.com:GlobalArtInc/warface.git
     cd $DIR/wfs
 fi
 
 
-if ! [ -f $DIR/.npm_i_api ]; then
-    echo "Installing API Dependencies"
-    cd $DIR/wfs/api && npm i && \
-    touch $DIR/.npm_i_api
-fi
-
-if ! [ -f $DIR/.npm_i_discord ]; then
-    echo "Installing Discord Dependencies"
-    cd $DIR/wfs/discord && npm i && \
-    touch $DIR/.npm_i_discord
-fi
-
-if ! [ -f $DIR/.npm_i_tg ]; then
-    echo "Installing TelegramBot Dependencies"
-    cd $DIR/wfs/telegram && npm i && \
-    touch $DIR/.npm_i_tg
-fi
-
-if ! [ -f $DIR/.npm_i_front ]; then
-    echo "Installing Front Dependencies"
-    cd $DIR/wfs/front && npm i && \
-    touch $DIR/.npm_i_front
+if ! [ -f $DIR/.npm_i_app ]; then
+    echo "Installing APP Dependencies"
+    cd $DIR/wfs/app && pnpm i && \
+    touch $DIR/.npm_i_app
 fi
