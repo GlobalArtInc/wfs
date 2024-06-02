c1a243cfc522b6733eb5b832981895f54bb7b392
Roman Khabarov <roman@globalart.dev>
2024-06-02 13:22:37 +0000

    fix

--- app/libs/shared/src/translation/translation.module.ts
+++ app/libs/shared/src/translation/translation.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ClientsModule } from '@nestjs/microservices';
 
 import { ClassTranslatorModule } from '../modules/class-translator';
 import { classTranslatorModuleConfig } from '../configs/class-translator.config';
-import { REDIS_PROVIDER } from '../configs/redis-microservice.config';
-
-const redisClientModule = ClientsModule.registerAsync([
-  {
-    name: REDIS_PROVIDER,
-    useFactory: (configService: ConfigService) => configService.get('redis-microservice'),
-    inject: [ConfigService],
-  },
-]);
 
 @Module({
-  imports: [redisClientModule, ClassTranslatorModule.forRootAsync(classTranslatorModuleConfig)],
+  imports: [ClassTranslatorModule.forRootAsync(classTranslatorModuleConfig)],
 })
 export class TranslationModule {}
