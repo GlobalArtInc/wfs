0f07b6d563e7e119efd45e0973eb7fb90f07073a
Roman Khabarov <roman@ga-games.com>
2024-06-05 09:00:20 +0200

    fix

--- .helm/def.sh
+++ .helm/def.sh
@@ -1,8 +1,4 @@
-# ENVNAME 
-#   prod - конфигурация "Продакшин", приложение смотрит на боевой сервер.
-#   dev* - конфигурация для разработки, запускаются база и пгадмин 
-
-export TAG=1.257
+export TAG=1.001
 export APPNAME=wfs
 export NAMESPACE=wfs
 
